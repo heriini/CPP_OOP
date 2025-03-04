424281c077d998ecfa250b7e91dfd4ddfc275c7c
heriiini <iniherii@gmail.com>
2025-03-04 11:27:13 +0700

    add folder 06_memory

---
+++ 06_memory/src/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+
+// kelas dengan constructor
+class Constructor{
+    public:
+        std::string data;
+
+        Constructor(const char* data){
+            Constructor::data = data;
+        }
+
+        void show(){
+            std::cout << "ini " << Constructor::data << std::endl;
+        }
+};
+
+// kelas tanpa constructor
+class WithoutConstructor{
+    public:
+        std::string data; //32 byte
+        // int data1;       // 4 byte
+
+        // void show(){
+        //     std::cout << "ini " << WithoutConstructor::data << std::endl;
+        // }
+
+};
+
+int main(){
+
+    // 1. membuat object tanpa constructor
+    // WithoutConstructor object1;
+    // object1.data = "tanpa constructor";
+    // object1.show();
+
+    // // 2. membuat object dengan constructor
+    // Constructor object2 = Constructor("dengan constructor");
+    // object2.show();
+
+    // // 3. membuat object dengan constructor (cara lain)
+    // Constructor object3("juga dengan constructor");
+    // object3.show();
+
+    // // 4. membuat object pada heap memory
+    // Constructor* object4 = new Constructor("dengan constructor tapi di heap momory");
+    // object4->show();
+
+    // Constructor* object5;
+    // object5 = new Constructor("dengan constructor tapi di heap momory");
+    // object5->show();
+
+    std::cout << "Alokasi class Constructor : " << sizeof(WithoutConstructor) << " byte" << std::endl;
+
+    std::string a,b,c,d;
+
+    std::cout << "\nAddress stack" << std::endl;
+    std::cout << &a << std::endl;
+    std::cout << &b << std::endl;
+    std::cout << &c << std::endl;
+    std::cout << &d << std::endl;
+
+
+    return 0;
+}
