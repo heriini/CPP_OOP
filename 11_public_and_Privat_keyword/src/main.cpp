ea8cd6b680e14ece389c2135fb988c5918ef9f38
heriiini <iniherii@gmail.com>
2025-03-12 20:10:58 +0700

    modified syntax at main.cpp

--- 11_public_and_Privat_keyword/src/main.cpp
+++ 11_public_and_Privat_keyword/src/main.cpp
@@ -7,14 +7,17 @@
 int main(){
 
     Mahasiswa* mahasiswa1 = new Mahasiswa("Suwardi");
-    std::cout << "Nama mahasiswa : " << mahasiswa1->namePublic << std::endl;
+    mahasiswa1->showDisplay();
+    delete mahasiswa1;
     
     Mahasiswa* mahasiswa2 = new Mahasiswa("John");
     mahasiswa2->showDisplay();
+    delete mahasiswa2;
 
     Mahasiswa* mahasiswa3 = new Mahasiswa("Rozak");
     mahasiswa3->showDisplay();
-
+    delete mahasiswa3;
+    
     return 0;
 }
     
